d0cb9559ad3ec289cff4c1e5906650106e23da40
ChenHuang <alan.tolearn@gmail.com>
2020-05-22 19:17:22 -0400

    Add API: create page

--- handler-test.js
+++ handler-test.js
@@ -1,11 +1,31 @@
 require('dotenv').config()
 const handler = require('./handler')
 
-console.log(
+const createPage = () => {
+  handler.createPage(
+    {
+      body: JSON.stringify({
+        title: 'Full text search',
+        url: 'https://www.google.com/search?sxsrf=ALeKk03JZyYEyu12ZowN8eOKpHmIaekECw%3A1590187253571&source=hp&ei=9VTIXtftIJ-bjLsP5JiBCA&q=full+text+search&oq=full+text+search&gs_lcp=CgZwc3ktYWIQAzIECCMQJzIECCMQJzIECCMQJzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAA6BwgjEOoCECc6BQgAEJECOgUIABCDAToICAAQgwEQkQJQmgpYkzBg7DFoCHAAeAGAAbsCiAHCHpIBCDAuMTYuNS4ymAEAoAEBqgEHZ3dzLXdperABCg&sclient=psy-ab&ved=0ahUKEwiXvP6SxcjpAhWfDWMBHWRMAAEQ4dUDCAk&uact=5',
+        content:
+          'Accessibility Links Skip to main content Accessibility help Accessibility feedback .. Search Modes All Videos Images News Shopping More SettingsTools About 11,470,000,000 results',
+        visited_at: new Date()
+      }),
+      pathParameters: {},
+    }, // event
+    {}, //content
+    function (data, ss) {
+      //callback function with two arguments
+      console.log(data, ss)
+    }
+  )
+}
+
+const search = () => {
   handler.search(
     {
       body: JSON.stringify({
-        query: 'test|one|two',
+        query: 'skip',
         limit: 10
       }),
       pathParameters: {
@@ -17,4 +37,6 @@ console.log(
       console.log(data, ss)
     }
   )
-)
+}
+
+search()
--- handler.js
+++ handler.js
@@ -1,6 +1,17 @@
 const connection = require('./models/index')
 const { successResponse, errorResponse } = require('./handler-helpers')
 
+module.exports.createPage = async (event) => {
+  try {
+    const body = JSON.parse(event.body)
+    const { Page } = await connection()
+    const result = await Page.create(body)
+    return successResponse(result)
+  } catch (err) {
+    return errorResponse(err, event)
+  }
+}
+
 module.exports.search = async (event) => {
   try {
     const resultsLimit = 10
