a745f2f04064a4a2d87cda6b53e22d9df3681f17
ChenHuang <alan.tolearn@gmail.com>
2020-05-24 13:10:33 -0400

    Update db search dictionary and configuration

--- migrations/20200521023222-add-full-text-search-to-pages.js
+++ migrations/20200521023222-add-full-text-search-to-pages.js
@@ -1,5 +1,4 @@
 const vectorName = '_search'
-
 const searchObjects = {
   pages: ['title', 'content'],
 }
@@ -9,31 +8,51 @@ module.exports = {
     queryInterface.sequelize.transaction((t) =>
       Promise.all(
         Object.keys(searchObjects).map((table) =>
+          // Add tsvector column
           queryInterface.sequelize
             .query(
               `
-          ALTER TABLE ${table} ADD COLUMN ${vectorName} TSVECTOR;
-        `,
+              ALTER TABLE ${table} ADD COLUMN ${vectorName} TSVECTOR;
+              `,
               { transaction: t }
             )
+            // Create custom search dictionary and configuration
             .then(() =>
               queryInterface.sequelize.query(
                 `
-                UPDATE ${table} SET ${vectorName} = to_tsvector('english', ${searchObjects[
-                  table
-                ].join(" || ' ' || ")});
-              `,
+                CREATE TEXT SEARCH DICTIONARY simple_english
+                  (TEMPLATE = pg_catalog.simple, STOPWORDS = english);
+
+                CREATE TEXT SEARCH CONFIGURATION simple_english
+                  (copy = english);
+                ALTER TEXT SEARCH CONFIGURATION simple_english
+                  ALTER MAPPING FOR asciihword, asciiword, hword, hword_asciipart, hword_part, word
+                  WITH simple_english;
+                `,
                 { transaction: t }
               )
             )
+            // Update tsvector column based on url and content
+            .then(() =>
+              queryInterface.sequelize.query(
+                `
+                UPDATE ${table} SET ${vectorName} = to_tsvector('simple_english', ${searchObjects[
+                  table
+                ].join(" || ' ' || ")});
+                `,
+                { transaction: t }
+              )
+            )
+            // Add index to tsvector column
             .then(() =>
               queryInterface.sequelize.query(
                 `
                 CREATE INDEX ${table}_search ON ${table} USING gin(${vectorName});
-              `,
+                `,
                 { transaction: t }
               )
             )
+            // Add trigger to update tsvector column on create and update
             .then(() =>
               queryInterface.sequelize.query(
                 `
@@ -42,7 +61,7 @@ module.exports = {
                 FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger(${vectorName}, 'pg_catalog.english', ${searchObjects[
                   table
                 ].join(', ')});
-              `,
+                `,
                 { transaction: t }
               )
             )
@@ -55,26 +74,39 @@ module.exports = {
     queryInterface.sequelize.transaction((t) =>
       Promise.all(
         Object.keys(searchObjects).map((table) =>
+          // Remove trigger
           queryInterface.sequelize
             .query(
               `
-          DROP TRIGGER ${table}_vector_update ON ${table};
-        `,
+              DROP TRIGGER ${table}_vector_update ON ${table};
+              `,
               { transaction: t }
             )
+            // Remove index
             .then(() =>
               queryInterface.sequelize.query(
                 `
                 DROP INDEX ${table}_search;
-              `,
+                `,
                 { transaction: t }
               )
             )
+            // Drop search dictionary and configuration
+            .then(() =>
+              queryInterface.sequelize.query(
+                `
+                DROP TEXT SEARCH CONFIGURATION simple_english;
+                DROP TEXT SEARCH DICTIONARY simple_english;
+                `,
+                { transaction: t }
+              )
+            )
+            // Remove tsvector column
             .then(() =>
               queryInterface.sequelize.query(
                 `
                 ALTER TABLE ${table} DROP COLUMN ${vectorName};
-              `,
+                `,
                 { transaction: t }
               )
             )
